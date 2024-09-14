277798a84e77a9e966e05febf8a76536c9509f80
Cendy <cendyemail@gmail.com>
2024-09-14 11:00:05 +0700

    fix: auto clear world can't destory block under maindoor bedrock

--- src/bot/features/auto_clear_world.rs
+++ src/bot/features/auto_clear_world.rs
@@ -13,10 +13,29 @@ pub fn start(bot: &Arc<Bot>) {
         (world.width, world.height)
     };
 
+    {
+        let position = {
+            let position = bot.position.read().unwrap();
+            position.clone()
+        };
+        while bot::is_inworld(&bot) {
+            let (foreground_id, background_id) = {
+                let world = bot.world.read().unwrap();
+                let tile = world.get_tile((position.x / 32.0) as u32, (position.y / 32.0 + 2.0) as u32).unwrap();
+                (tile.foreground_item_id, tile.background_item_id)
+            };
+
+            if background_id != CAVE_BACKGROUND || foreground_id == BEDROCK {
+                break;
+            }
+            punch(&bot, 0, 2);
+            thread::sleep(Duration::from_millis(350));
+        }
+    }
+
     for y in 23..world_height - 6 {
         for x in 0..world_width {
             while bot::is_inworld(&bot) {
-
                 let (foreground_id, background_id) = {
                     let world = bot.world.read().unwrap();
                     if let Some(tile) = world.get_tile(x, y) {
