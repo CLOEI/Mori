eaae5ca0bcc000c9d0b609a3e6f3d9edd840611d
Cendy <cendyemail@gmail.com>
2025-09-12 14:21:47 +0700

    misc: update analysis & add fcmtoken

--- core/src/types/login_info.rs
+++ core/src/types/login_info.rs
@@ -34,42 +34,44 @@ pub struct LoginInfo {
     pub wk: String,
     pub aat: String,
     pub ltoken: String,
+    pub fcm_token: String,
 }
 
 impl LoginInfo {
     pub fn new() -> Self {
         let mut login_info = LoginInfo {
-            uuid: String::new(),
             tank_id_name: String::new(),
             tank_id_pass: String::new(),
-            protocol: "216".to_string(),
-            fhash: "-716928004".to_string(),
-            mac: utils::random::mac_address(),
-            requested_name: "".to_string(),
-            hash2: String::new(),
-            fz: "20743704".to_string(),
             f: "1".to_string(),
-            player_age: "20".to_string(),
+            protocol: "217".to_string(),
             game_version: "5.27".to_string(),
-            lmode: "1".to_string(),
-            cbits: "1040".to_string(),
-            rid: utils::random::hex(32, true),
+            fz: "22243512".to_string(),
+            cbits: "1024".to_string(),
+            player_age: "20".to_string(),
             gdpr: "2".to_string(),
-            hash: "0".to_string(),
+            fcm_token: String::new(),
             category: "_-5100".to_string(),
-            token: String::new(),
             total_play_time: "0".to_string(),
-            door_id: String::new(),
             klv: String::new(),
+            hash2: String::new(),
             meta: String::new(),
+            fhash: "-716928004".to_string(),
+            rid: utils::random::hex(32, true),
             platform_id: "0,1,1".to_string(),
             device_version: "0".to_string(),
-            zf: "31631978".to_string(),
             country: "jp".to_string(),
-            user: String::new(),
+            mac: utils::random::mac_address(),
             wk: utils::random::hex(32, true),
-            aat: "0".to_string(),
+            zf: "31631978".to_string(),
+            user: String::new(),
+            uuid: String::new(),
             ltoken: "".to_string(),
+            hash: "0".to_string(),
+            requested_name: "".to_string(),
+            lmode: "1".to_string(),
+            token: String::new(),
+            door_id: String::new(),
+            aat: "0".to_string(),
         };
 
         login_info.klv = utils::proton::generate_klv(
@@ -93,7 +95,7 @@ impl LoginInfo {
 
     pub fn to_string(&self) -> String {
         format!(
-            "tankIDName|{}\ntankIDPass|{}\nrequestedName|{}\nf|{}\nprotocol|{}\ngame_version|{}\nfz|{}\ncbits|{}\nplayer_age|{}\nGDPR|{}\ncategory|{}\ntotalPlaytime|{}\nklv|{}\nhash2|{}\nmeta|{}\nfhash|{}\nrid|{}\nplatformID|{}\ndeviceVersion|{}\ncountry|{}\nhash|{}\nmac|{}\nwk|{}\nzf|{}\n",
+            "tankIDName|{}\ntankIDPass|{}\nrequestedName|{}\nf|{}\nprotocol|{}\ngame_version|{}\nfz|{}\ncbits|{}\nplayer_age|{}\nGDPR|{}\nFCMToken|{}\n\ncategory|{}\ntotalPlaytime|{}\nklv|{}\nhash2|{}\nmeta|{}\nfhash|{}\nrid|{}\nplatformID|{}\ndeviceVersion|{}\ncountry|{}\nhash|{}\nmac|{}\nwk|{}\nzf|{}lmode={}\n",
             self.tank_id_name,
             self.tank_id_pass,
             self.requested_name,
@@ -104,6 +106,7 @@ impl LoginInfo {
             self.cbits,
             self.player_age,
             self.gdpr,
+            self.fcm_token,
             self.category,
             self.total_play_time,
             self.klv,
@@ -117,7 +120,8 @@ impl LoginInfo {
             self.hash,
             self.mac,
             self.wk,
-            self.zf
+            self.zf,
+            self.lmode,
         )
     }
 }
--- frida/patch_memory_protection.js
+++ frida/patch_memory_protection.js
@@ -3,5 +3,7 @@ const ZwProtectVirtualMemoryAddress = module.findExportByName("ZwProtectVirtualM
 console.log("ZwProtectVirtualMemory address: " + ZwProtectVirtualMemoryAddress);
 
 const patchBytes = [0x4C, 0x8B, 0xD1, 0xB8, 0x50];
+const patchSize = patchBytes.length;
 
+Memory.protect(ZwProtectVirtualMemoryAddress, patchSize, 'rwx');
 ZwProtectVirtualMemoryAddress.writeByteArray(patchBytes);
